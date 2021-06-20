c50a47c23465cbf91b8744cf4d28a2dd2da8e6b0
Irocool <ikuligina@gmail.com>
2021-06-20 20:57:10 +0300

    init

---
+++ GBShop/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GBShop
+//
+//  Created by Пользователь on 20.06.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
